ebdc2d050b402b2f4a2634e2d1152152bcea6ba6
NIGAM YADAV <71872897+nigamyadav72@users.noreply.github.com>
2025-06-30 09:18:52 +0545

    database changed to mysql

--- django_rest_main/settings.py
+++ django_rest_main/settings.py
@@ -81,8 +81,13 @@ WSGI_APPLICATION = 'django_rest_main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DRF',
+        'USER':'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
     }
 }
 
